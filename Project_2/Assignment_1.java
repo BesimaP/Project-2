a587aa8182512aab9f3c2d9a42383522dd10c180
BesimaP <bepl1000@stud.ek.dk>
2026-02-01 21:06:03 +0100

    First assignment done

---
+++ Project_2/Assignment_1.java
@@ -0,0 +1,273 @@
+void main() {
+    /*Opgave 1.1: Din første if statement
+    int age = 20;
+    if (age>=18){;
+        System.out.println("Du er myndig");
+    }*/
+
+    /*Opgave 1.2: Temperature check
+    int temperature = 30;
+    if (temperature >= 25) {
+        System.out.println("Det er varmt");
+    } */
+
+    /*Opgave 1.3: Score check
+    int score = 85;
+    if (score >= 60){
+        System.out.println("Bestået");
+    } */
+
+    /*Opgave 1.4: Pass or fail
+    int score = 55;
+    if (score >=60){
+        System.out.println("PASS");
+    } else {
+        System.out.println("FAIL");
+    } */
+
+    /*Opgave 1.5: Adult or minor
+    int age = 16;
+    if (age >=18){
+        System.out.println("Adult");
+    } else {
+        System.out.println("Minor");
+    }*/
+
+    /*Opgave 1.6: Positive or negative
+    int number = -5;
+    if (number >=0){
+        System.out.println("Positive");
+    } else {
+        System.out.println("Not positive");
+    }*/
+
+    /*Opgave 1.7: Even or odd
+    int number = 7;
+    if (number % 2 == 0){
+        System.out.println("Even");
+    } else {
+        System.out.println("Odd");
+    }*/
+
+    /*Opgave 1.8: Price discount
+    int price = 0;
+    if (price >500){
+        System.out.println("discount available");
+    } else {
+        System.out.println("No discount");
+    } */
+
+    /*Opgave 1.9: Stock availability
+    int stock = 0;
+    if (stock >0){
+        System.out.println("Available");
+    } else {
+        System.out.println("Out of stock");
+    }*/
+
+    /*Opgave 1.10: Speed limit
+    int speed = 135;
+    if (speed > 130){
+        System.out.println("Speeding!");
+    } else {
+        System.out.println("Within limit");
+    }*/
+
+    /*Opgave 11: Letter grades
+    int score = 85;
+    if (score >= 90){
+        System.out.println("A");
+    } else if (score >= 80) {
+        System.out.println("B");
+    } else if (score >= 70) {
+        System.out.println("C");
+    } else if (score >= 60) {
+        System.out.println("D");
+    } else {
+        System.out.println("F");
+    }*/
+
+    /*Opgave 12: Flere karakter
+    int score = 95;
+    if (score >= 90){
+        System.out.println("A");
+    } else if (score >= 80) {
+        System.out.println("B");
+    } else if (score >= 70) {
+        System.out.println("C");
+    } else if (score >= 60) {
+        System.out.println("D");
+    } else {
+        System.out.println("F");
+    }
+
+    int score1 = 72;
+    if (score1 >= 90){
+        System.out.println("A");
+    } else if (score1 >= 80) {
+        System.out.println("B");
+    } else if (score1 >= 70) {
+        System.out.println("C");
+    } else if (score1 >= 60) {
+        System.out.println("D");
+    } else {
+        System.out.println("F");
+    }
+
+    int score2 = 58;
+    if (score2 >= 90){
+        System.out.println("A");
+    } else if (score2 >= 80) {
+        System.out.println("B");
+    } else if (score2 >= 70) {
+        System.out.println("C");
+    } else if (score2 >= 60) {
+        System.out.println("D");
+    } else {
+        System.out.println("F");
+    } */
+
+    /*Opgave 13: Age categories
+    int age = 10;
+    if (age <13){
+        System.out.println("Child");
+    } else if (age <18) {
+        System.out.println("Teen");
+    } else if (age <65) {
+        System.out.println("Adult");
+    } else if (age >=65) {
+        System.out.println("Senior");
+    } */
+
+    /*Opgave 14: temperature ranges
+    int temp = 22;
+    if (temp <0) {
+        System.out.println("Freezing");
+    } else if (temp < 15) {
+        System.out.println("Cold");
+    } else if (temp < 25) {
+            System.out.println("Comfortable");
+    } else if (temp >= 25) {
+            System.out.println("Hot");
+    }*/
+
+    /* Opgave 15: Ticket pricing
+    int age = 25;
+    if (age < 12){
+        System.out.println("50 kr");
+    } else if (age < 18){
+        System.out.println("75 kr");
+    } else if (age < 65){
+        System.out.println("100 kr");
+    } else if (age >=65){
+        System.out.println("60 kr");
+    }*/
+
+    /* Opgave 16: Grade ranges
+    int score = 88;
+    String grade;
+
+    if (score >= 90) {
+        grade = "A";
+    } else if (score >=80){
+        grade = "B";
+    } else if (score >70) {
+        grade = "C";
+    } else if (score >60){
+        grade = "D";
+    } else {
+        grade = "F";
+    }
+    System.out.println("Grade: "+ grade);
+    */
+
+    /*Opgave 1.17: Password check
+    String password ="secret123";
+    if (password.equals("secret123")){
+        System.out.println("Access granted");
+    } else {
+        System.out.println("Access denied");
+    }*/
+
+    /*Opgave 1.18: Case sensitivity
+    String word1 = "Hello";
+    String word2 = "hello";
+    if (word1.equals(word2)) {
+        System.out.println("Match");
+    } else {
+        System.out.println("No match");
+    }*/
+
+    /*Opgave 1.19: Multiple password attempts
+    String correctPassword = "secret123";
+
+    String attempt1 = "Wrong";
+    if (attempt1.equals(correctPassword)){
+        System.out.println("Correct");
+    } else {
+        System.out.println("Wrong");
+    }
+
+    String attempt2 = "Secret123";
+    if (attempt2.equals(correctPassword)){
+        System.out.println("Correct");
+    } else {
+        System.out.println("Wrong");
+    }
+
+    String attempt3 = "secret123";
+    if (attempt3.equals(correctPassword)){
+        System.out.println("Correct");
+    } else {
+        System.out.println("Wrong");
+    }*/
+
+    /*Opgave 1.20: Day of week
+    String day = "Monday";
+    if (day.equals("Saturday") || day.equals("Sunday")){
+        System.out.println("Weekend");
+    } else {
+        System.out.println("Weekday");
+    }*/
+
+    /*Opgave 1.21: Command processor
+    String command = "start";
+    if (command.equals("start")){
+        System.out.println("Starting...");
+    } else if (command.equals("stop")){
+        System.out.println("Stopping...");
+    } else if (command.equals("pause")){
+        System.out.println("Pausing...");
+    } else {
+        System.out.println("Unkown command");
+    }*/
+
+    /*Opgave 1.23: Boolean flag
+    boolean isActive = true;
+    if (isActive){
+        System.out.println("System ON");
+    } else {
+        System.out.println("System OFF");
+    }*/
+
+    /*Opgave 1.24: Combining conditions
+    boolean hasLicense = true;
+    int age = 20;
+    if (age >= 19 && hasLicense) {
+        System.out.println("can drive");
+    } else {
+        System.out.println("Cannot drive");
+    }*/
+
+    /*Opgave 1.25: Setting booelan based on condition
+    int score = 75;
+    boolean passed;
+
+    if (score >=60){
+        passed = true;
+    } else {
+        passed = false;
+    }
+    System.out.println("Passed: " + passed);
+     */
+}
